1e4a2527054bebc1aa512ad7670704f1e65b160f
Trenton Telge <darkforce100@users.noreply.github.com>
2018-01-16 13:42:02 -0600

    Added Settings activity
    Signed-off-by: Trenton Telge <darkforce100@users.noreply.github.com>

---
+++ app/src/main/java/com/eventhorizonwebdesign/chandownlaoder/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.eventhorizonwebdesign.chandownlaoder
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
